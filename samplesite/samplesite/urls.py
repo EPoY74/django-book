2fe55cbca584cff29820730885fc88c2729d9a86
Evgenii Petrov Linux IceBook <epoy74@gmail.com>
2025-11-10 23:22:59 +0500

    feat(bboard): create app and add URL pattern /bboard/

--- samplesite/samplesite/urls.py
+++ samplesite/samplesite/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from bboard import views
+
 urlpatterns = [
+    path('bboard/', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
