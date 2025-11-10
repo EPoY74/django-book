1b15d7abd44043d7616ccc1e06e9cc8da66e492b
Evgenii Petrov Linux IceBook <epoy74@gmail.com>
2025-11-10 23:52:33 +0500

    feat(bboard): include bboard.urls at /bboard/

--- samplesite/samplesite/urls.py
+++ samplesite/samplesite/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from bboard import views
 
 urlpatterns = [
-    path('bboard/', views.index, name='index'),
+    path('bboard/', include('bboard.urls', namespace="bboard")),
     path('admin/', admin.site.urls),
 ]
