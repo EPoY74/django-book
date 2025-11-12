a2a7518723b28000386f30511b30f343bb7524c0
Evgenii Petrov Linux IceBook <epoy74@gmail.com>
2025-11-12 08:20:18 +0500

    test(bboard): add smoke test for index view (200 & URL)

--- samplesite/bboard/tests.py
+++ samplesite/bboard/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
-
 # Create your tests here.
+
+from django.test import TestCase
+from django.urls import reverse
+
+
+class IndexViewTests(TestCase):
+    def test_index_returns_200_and_has_corect_url(self):
+        url = reverse('bboard:index')
+        # Check that the URL is correct
+        self.assertEqual(url, '/bboard/')
+        resp = self.client.get(url)
+        # Check that the response is 200 OK.
+        self.assertEqual(resp.status_code, 200)
