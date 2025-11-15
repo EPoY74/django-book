7bfe969e21fa89e983632cbe766a09bf1ed8f1f5
Evgenii Petrov Linux IceBook <epoy74@gmail.com>
2025-11-15 23:47:29 +0500

    feat: (views): replace plain HttpResponse with template rendering

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.template import loader
 
 # from django.shortcuts import render
 from .models import Bb
@@ -6,11 +7,9 @@ from .models import Bb
 # Create your views here.
 
 def index(request):
-    s = "Объявления\r\n\r\n\r\n"
-
-    for bb in Bb.objects.order_by('-published'):
-        s += f"{bb.title}\r\n{bb.content}\r\n\r\n"
-
-    return HttpResponse(s, content_type="text/plain; charset=utf-8")
+    template = loader.get_template('bboard/index.html')
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs': bbs}
+    return HttpResponse(template.render(context, request))
 
 
