6103c2accad68ba68210835d58442fb4b6a9c774
Evgenii Petrov Linux IceBook <epoy74@gmail.com>
2025-11-18 08:08:37 +0500

    feat(models): add Russian verbose names for fields

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Bb(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField()
-    price = models.FloatField()
-    published = models.DateTimeField(auto_now_add=True, db_index=True)
+    title = models.CharField(max_length=50, verbose_name="Заголовок")
+    content = models.TextField(verbose_name="описание")
+    price = models.FloatField(verbose_name="цена", default=0)
+    published = models.DateTimeField(auto_now_add=True, 
+                                     db_index=True,
+                                     verbose_name="дата публикации"
+                                     )
